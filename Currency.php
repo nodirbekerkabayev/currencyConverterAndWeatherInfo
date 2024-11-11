6e33e75f731ddfb0f45a8c9f2123de4bea56f4dc
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2024-11-11 20:43:42 +0500

    patch changed

---
+++ Currency.php
@@ -0,0 +1,21 @@
+<?php
+
+const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
+
+class Currency
+{
+
+    public function __construct()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, CURRENCY_API_URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        $decoded = json_decode($output);
+        echo $decoded[0]->Ccy;
+    }
+
+}
