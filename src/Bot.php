649f68849af69a5e82febb92dd610d7eb22f0a37
nodirbekerkabayev <erkabayevnodirbek@gmail.com>
2024-11-20 11:15:52 +0500

    change cURL to guzzle package

--- src/Bot.php
+++ src/Bot.php
@@ -1,16 +1,22 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
 class Bot{
+
+    public $client;
     const API_URL = 'https://api.telegram.org/bot';
     private $token = '7662222216:AAGZb9JBcIA3CTGz7yJve5EuLLd3LrOL7Xk';
 
     public function makeRequest($method, $data = []){
-        $ch = curl_init(self::API_URL . $method);
-        curl_setopt($ch, CURLOPT_URL, self::API_URL . $this->token . '/' . $method);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($response);
+        $this->client = new Client([
+            'base_uri' => self::API_URL . $this->token,
+            'timeout'  => 2.0,
+            'verify' => false,
+        ]);
+        $request = $this->client->request('POST', '/' . $method, $data);
+        $response = $request->getBody()->getContents();
+        return $response;
     }
 }
--- src/Currency.php
+++ src/Currency.php
@@ -1,21 +1,25 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
 const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
 
 class Currency
 {
+    public $client;
     public array $currencies = [];
-
     public function __construct()
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, CURRENCY_API_URL);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $output = curl_exec($ch);
-        curl_close($ch);
-
-        $this->currencies = json_decode($output);
+        $this->client = new Client([
+            'base_uri' => CURRENCY_API_URL,
+            'timeout'  => 2.0,
+            'verify' => false
+        ]);
+        $request = $this->client->request('GET', '');
+        $this->currencies = json_decode($request->getBody()->getContents());
+        return $this->currencies;
 
     }
 
